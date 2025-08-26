ba82455279f4d4dc35a72c592c1b1d3a947d8a8d
Anuj Dutta <ytzpn4@gmail.com>
2025-08-27 01:05:45 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,8 @@
+a=int(input("Enter a number""==> "))
+b=int(input("Enter another number""==>"))
+add=(a+b)
+sub=(a-b)
+mul=(a*b)
+div=(a/b)
+
+print("Addition:", add,"\nSubstraction:",sub,"\nMultiplication:",mul,"\nDivison:",div)
